7aee8132e9817ae0961c1826ce017cb7da0ef2ff
Makume <tentemakume@gmail.com>
2024-04-02 13:42:32 -0700

    advanced tasks

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -s -X PUT -d "user_id=98" -H "Origin: You got me!" -L 0.0.0.0:5000/catch_me
+# Script: 0-catch_me.sh
+# Description: Makes a request to 0.0.0.0:5000/catch_me and displays the response
+
+# Send a request to the server
+curl -sLX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
+
+# The server will respond with "You got me!" in the body of the response
+
